6297d5e34d140909e45c27410864298f92930958
안은결 <eungyeole@naver.com>
2024-03-20 01:47:45 +0900

    chore: initial commit

---
+++ src/manifest/make-manifest.ts
@@ -0,0 +1,22 @@
+import * as fs from "node:fs";
+import { resolve } from "node:path";
+import ManifestParser from "./parser.js";
+import { logger } from "@rsbuild/core";
+
+export const makeManifest = async (
+  manifest: chrome.runtime.ManifestV3,
+  to: string
+) => {
+  if (!fs.existsSync(to)) {
+    fs.mkdirSync(to);
+  }
+
+  const toManifest = resolve(to, "manifest.json");
+
+  fs.writeFileSync(
+    toManifest,
+    ManifestParser.convertManifestToString(manifest.default)
+  );
+
+  logger.log(`Manifest file created`);
+};
