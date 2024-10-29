12af42f059000ab3c185184d76335e2fc3f86f2a
Eungyeol <eungyeole@naver.com>
2024-10-30 01:44:47 +0900

    chore: changesets settings

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,18 +1,22 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
+import * as path from "node:path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'rsbuild-plugin-web-extension',
-  description: 'rbsuild plugin for chrome/web extension',
-  icon: '/rspress-icon.png',
+  root: path.join(__dirname, "docs"),
+  title: "rsbuild-plugin-web-extension",
+  description: "rbsuild plugin for chrome/web extension",
+  icon: "/rspress-icon.png",
   logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
+    light: "/rspress-light-logo.png",
+    dark: "/rspress-dark-logo.png",
   },
   themeConfig: {
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/web-infra-dev/rspress",
+      },
     ],
   },
 });
